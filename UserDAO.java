b20fb8ae43990e7dfe596bff9e5dcf189d7db651
KAGITHALASOWMYA <kagithalasowmya2001@gmail.com>
2024-11-18 22:42:31 +0530

    UserDAO.java

---
+++ UserDAO.java
@@ -0,0 +1,11 @@
+package com.codegnan.dao;
+import java.util.List;
+import com.codegnan.entity.User;
+ 
+public interface UserDAO {
+	void addUser(User user);
+	User getUserById(int id);
+	List<User> getAllUsers();
+	void updateUser(User user);
+	void deleteUser(int id);
+}
